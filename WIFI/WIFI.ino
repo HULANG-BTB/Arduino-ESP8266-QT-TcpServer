8f093b379dc0c4397a26df1dab0b3fe32a5aca16
HULANG-BTB <774023581@qq.com>
2019-03-05 17:49:38 +0800

    修改运行方式-增加连接检测，每分钟检查一次，连接断开时自动重新连接

--- WIFI/WIFI.ino
+++ WIFI/WIFI.ino
@@ -1,14 +1,24 @@
 #include <SoftwareSerial.h>
 #include <string.h>
+#include <Metro.h>
 SoftwareSerial Wifi(7, 6);
 
 String OpenCmd = "wake";
 const int OpenPin = 16;
 
+Metro CheckLinkStatus = Metro(60000); // 周期 1分钟
+Metro GetWifiCmd = Metro(0); // 周期 0
+
+void initEsp8266();
+void wake();
+void connectToWifi();
+void loginToServer();
+bool tcpStatus();
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  Wifi.begin(115200);
+  Wifi.begin(9600);
   pinMode(12, OUTPUT);
   digitalWrite(12, HIGH);
   pinMode(2, OUTPUT);
@@ -16,19 +26,7 @@ void setup() {
   pinMode(OpenPin, OUTPUT);
   digitalWrite(OpenPin, HIGH);
   Serial.println("System is OK！");
-  delay(5000);
-  Wifi.println("AT+RST");
-  delay(5000);
-  Wifi.println("AT+CWMODE=1");
-  delay(3000);
-  Wifi.println("AT+CIPMUX=0");
-  delay(3000);
-  Wifi.println("AT+CIPSTART=\"TCP\",\"119.28.180.170\",8888");
-  delay(3000);
-  Serial.println("Wifi is OK！");
-  Wifi.println("AT+CIPSEND=15");
-  delay(500);
-  Wifi.println("WakeServerStart");
+  initEsp8266();
 }
 
 void wake() {
@@ -37,31 +35,82 @@ void wake() {
   digitalWrite(OpenPin, HIGH);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-    if ( Serial.available() ) {
-      String cmd;
-      while( Serial.available() ) {
-        cmd += (char)Serial.read();
-        delay(5);
-      }
-      cmd.trim();
-      Serial.println(cmd);
-      delay(100);
-      Wifi.println(cmd);
+void initEsp8266() {
+  Serial.println("Send AT+RST！");
+  Wifi.println("AT+RST");
+  Serial.println("Send AT+UART_DEF=9600,8,1,0,0！");
+  Wifi.println("AT+UART_DEF=9600,8,1,0,0");
+  delay(5000);
+  Serial.println("Send AT+CWMODE=1！");
+  Wifi.println("AT+CWMODE=1");
+  delay(1000);
+  Serial.println("Send AT+CIPMUX=0！");
+  Wifi.println("AT+CIPMUX=0");
+  delay(1000);
+  connectToWifi();
+  Serial.println("Wifi is OK！");
+}
+
+void connectToWifi() {
+  Serial.println("Disconnect From Wifi! ");
+  Wifi.println("AT+CWQAP");
+  delay(5000);
+  Serial.println("Connect To Wifi Xiaomi_A077 ! ");
+  Wifi.println("AT+CWJAP=\"Xiaomi_A077\",\"xiaomiwifi\"");
+  delay(5000);
+}
+
+void loginToServer() {
+  Serial.println("Disconnected From Server ! ");
+  Wifi.println("AT+CIPCLOSE");
+  delay(1000);
+  Serial.println("Connected To Server ! ");
+  Wifi.println("AT+CIPSTART=\"TCP\",\"119.28.180.170\",8888");
+  delay(5000);
+}
+
+bool tcpStatus() {
+  Serial.println("Send AT+CIPSTATUS ! ");
+  Wifi.println("AT+CIPSTATUS");
+  String result;
+  while(!Wifi.available());
+  while(Wifi.available()) {
+    result += (char)Wifi.read();
+    delay(5); 
+  }
+  result.trim();
+  if (strstr(result.c_str(), "STATUS:3") != NULL) {
+    return true;
+  }
+  return false;
+}
+
+void CheckStatus() {
+  Serial.println("Check Tcp Link Status ! ");
+  if (!tcpStatus()) {
+      loginToServer();
     }
-    if ( Wifi.available() ) {
+}
+
+void GetCmd() {
+  if ( Wifi.available() ) {
       String cmd;
       while( Wifi.available() ) {
           cmd += (char)Wifi.read();
           delay(5);  
       }
       cmd.trim();
-      Serial.println(cmd);
       delay(100);
       if( strstr(cmd.c_str(), OpenCmd.c_str()) != NULL ) {
-        Serial.println("yes");
         wake();
       }
     }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if ( CheckLinkStatus.check() )
+       CheckStatus();
+  if ( GetWifiCmd.check() )
+       GetCmd();
 }
